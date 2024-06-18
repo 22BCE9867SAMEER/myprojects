ebb640ebce9b08180449864b075bac87b58b0576
22BCE9867SAMEER <sameer.22bce9867@vitapstudent.ac.in>
2024-06-18 19:42:58 +0530

    MultiScreen

---
+++ app/src/main/java/com/sameer/multiscreen/ordersactivity.kt
@@ -0,0 +1,25 @@
+package com.sameer.multiscreen
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class ordersactivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_ordersactivity)
+        val text = findViewById<TextView>(R.id.textView3)
+      val ordersplaced =   intent.getStringExtra(MainActivity.KEY)
+        text.text = ordersplaced.toString()
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
